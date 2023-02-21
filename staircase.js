b3861cb8d91a0aa70f5cd455ea525fbe9ce045a8
satriayusuf <satriayusuf21@gmail.com>
2023-02-21 22:28:30 +0700

    hackerrank warmup

---
+++ staircase.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+    // Write your code here
+    for (let i = 0; i < n; i++) {
+    const tangga = Array(i + 1)
+      .fill("#")
+      .join("")
+      .padStart(n);
+    console.log(tangga);
+  }
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
